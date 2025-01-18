090cd78b9a7c118d5afafb7082ea6e15fa9aa92e
Vesel4ak <gushchinkv@yandex.ru>
2025-01-18 17:00:27 +0300

    Update link_data.py

--- link_data.py
+++ link_data.py
@@ -1,19 +1,19 @@
-import json #импортируем библиотеку json для работы с файлами формата json
-def open_file(link): #функция для открытия файла
+import json #import json
+def open_file(link): #func
     with open(link,"r") as file:
         links =set(file.read().splitlines())
     return links
-def write_file(links,link): #функция для изменения  файла
+def write_file(links,link): #func
     with open(link,"w",encoding ="utf-8") as file:
         file.write("\n".join(links))
-def open_json(jsfile):      #функция для открытия файла с помощью json
+def open_json(jsfile):      #func
     with open(jsfile,"r",encoding ="utf-8") as json_file:
         load = json.load(json_file)
     return load
-def write_json(data,jsfile): #функция для изменения  файла с помощью json
+def write_json(data,jsfile): #func
     with open(jsfile,"a",encoding ="utf-8") as jsonfile:
         json.dump(data,jsonfile)
-def sort(link1,link2): #функция для перебора ссылкок
+def sort(link1,link2): #func
     found =set()
     not_found =set()
     for link in link1:
@@ -24,9 +24,9 @@ def sort(link1,link2): #функция для перебора ссылкок
     for link in link2:
         if link not in link1:
             not_found.add(link)
-    write_json(list(found),"совпадения.json") #запускаем цикл программы для размещения ссылок по файлам
+    write_json(list(found),"совпадения.json") #launch
     write_json(list(not_found), "не_найдено.json")
 
-link1 = open_file("Ссылки1.txt") #ссылка №1
-link2 = open_file("Ссылки2.txt") #ссылка №2
-sort(link1,link2) #сортировка
+link1 = open_file("links1.txt") #link1
+link2 = open_file("links2.txt") #link2
+sort(link1,link2) #sort
